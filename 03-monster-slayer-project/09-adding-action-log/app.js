d754fa263d1a23d13a4454c1e334f5dd14d97f20
Raunak Hajela <raunakhajela@gmail.com>
2018-05-29 12:18:00 +0530

    09- adding action logs to display our attack data 10 - displaying our logs into index.html file 11 - refactoring logs and printing it for all attacks 12 - showing css based on attacks

--- 03-monster-slayer-project/09-adding-action-log/app.js
+++ 03-monster-slayer-project/09-adding-action-log/app.js
@@ -3,7 +3,8 @@
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function(){
@@ -12,13 +13,16 @@
       this.monsterHealth = 100;
     },
     attack: function(){
-      //monsterHealth damage
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if(this.checkWin()){
         return;
       }
 
-      //playerHealth damage
       this.playerHealth -= this.calculateDamage(5, 12); ;
       this.checkWin();
     },
@@ -41,8 +45,13 @@
       this.gameIsRunning = false;
     },
     monsterAttacks: function(){
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
       this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
     },
     calculateDamage: function(min, max){
       return Math.max(Math.floor(Math.random() * max) + 1, min);
--- 03-monster-slayer-project/12-styling-the-log/app.js
+++ 03-monster-slayer-project/12-styling-the-log/app.js
@@ -3,27 +3,36 @@
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function(){
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function(){
-      //monsterHealth damage
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+          isPlayer: true,
+          text: 'Player hits Monster for ' + damage
+      });
       if(this.checkWin()){
         return;
       }
 
-      //playerHealth damage
-      this.playerHealth -= this.calculateDamage(5, 12); ;
-      this.checkWin();
+      this.monsterAttacks();
     },
     specialAttack: function(){
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if(this.checkWin()){
         return;
       }
@@ -35,14 +44,23 @@
       }else{
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals Monster for 10'
+      });
       this.monsterAttacks();
     },
     giveUp: function(){
       this.gameIsRunning = false;
     },
     monsterAttacks: function(){
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
       this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
     },
     calculateDamage: function(min, max){
       return Math.max(Math.floor(Math.random() * max) + 1, min);
